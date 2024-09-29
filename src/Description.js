29a9df18f00d12426b5b4f5d84908985fe72b606
7R011 <7R011@protonmail.ch>
2024-09-28 21:00:55 -0400

    desc template

--- src/Description.js
+++ src/Description.js
@@ -1,14 +1,66 @@
 import "./App.css";
 import Footer from "./Footer";
-import db from "./Itemdb";
 
 function Description(props) {
-  console.log(props.item);
+  let item = props.item;
   return (
     <div className="App">
       <div className="App-header">
         <h2>{props.item.name}</h2>
-        <p className="blurb">{props.item.blurb}</p>
+        <p className="blurb">{item.blurb}</p>
+        {item.stats ? (
+          <table className="statsTable">
+            <tr>
+              <td className={item.stats.ql.quality}>
+                QL: {item.stats.ql.value}
+              </td>
+              <td>
+                DMG:{" "}
+                <span className={item.stats.minDamage.quality}>
+                  {item.stats.minDamage.value}
+                </span>
+                -
+                <span className={item.stats.maxDamage.quality}>
+                  {item.stats.maxDamage.value}
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <td className={item.stats.aSpeed.quality}>
+                AS: {item.stats.aSpeed.value}
+              </td>
+              <td className={item.stats.rSpeed.quality}>
+                RS: {item.stats.rSpeed.value}
+              </td>
+            </tr>
+            <tr>
+              <td className={item.stats.range.quality}>
+                Range: {item.stats.range.value}
+              </td>
+              <td className={item.stats.init.quality}>
+                Init: {item.stats.init.value}
+              </td>
+            </tr>
+            <tr>
+              <td className={item.stats.dual.quality}>
+                DW?: {item.stats.dual.value}
+              </td>
+              <td className={item.stats.specials.quality}>
+                SPs: {item.stats.specials.value}
+              </td>
+            </tr>
+            <tr>
+              <td className={item.stats.attackCap.quality}>
+                MBS: {item.stats.attackCap.value}
+              </td>
+              <td className={item.stats.multi.quality}>
+                Multi: {item.stats.multi.value}
+              </td>
+            </tr>
+          </table>
+        ) : (
+          <p>Fill out the stats bucko</p>
+        )}
       </div>
       <Footer />
     </div>
--- src/Itemdb.js
+++ src/Itemdb.js
@@ -12,7 +12,63 @@ const db = {
       min: 30,
       max: 50,
       type: "1hb",
-      blurb: "",
+      blurb: "Drops in TOTW",
+      stats: {
+        ql: {
+          value: "SPECIAL",
+          quality: "redBold",
+        },
+        dual: {
+          value: "Yes",
+          quality: "whiteBold",
+        },
+        aSpeed: {
+          value: "1s",
+          quality: "goldBold",
+        },
+        rSpeed: {
+          value: "1s",
+          quality: "goldBold",
+        },
+        range: {
+          value: "3m",
+          quality: "",
+        },
+        minDamage: {
+          value: "28",
+          quality: "",
+        },
+        maxDamage: {
+          value: "112",
+          quality: "whiteBold",
+        },
+        attackCap: {
+          value: "700",
+          quality: "goldBold",
+        },
+        init: {
+          value: "Melee",
+          quality: "",
+        },
+        multi: {
+          value: "112",
+          quality: "whiteBold",
+        },
+        specials: {
+          value: "Fast Attack",
+          quality: "",
+        },
+        reqs: {
+          "1hb": {
+            value: "305",
+            quality: "redBold",
+          },
+          "Fast Attack": {
+            value: "116",
+            quality: "",
+          },
+        },
+      },
     },
     "Skull of Misery/Despair": {
       name: "Skull of Misery/Despair",
