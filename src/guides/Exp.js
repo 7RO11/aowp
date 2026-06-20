f4e41b72d14460f8d4561f171608f876eb821055
7R011 <7R011@protonmail.ch>
2026-06-20 02:49:49 -0400

    xp first draft

--- src/guides/Exp.js
+++ src/guides/Exp.js
@@ -8,8 +8,23 @@ function Exp() {
       <p className="guideText">
         Once you've reached TL4, you may notice that the leveling begins to dry
         up. This guide seeks to provide the means of buffing your % Add. Xp stat
-        to reduce your time bashing rocks.
+        to reduce your time bashing rocks. Without a Bureaucrat or Advy, you'll
+        be looking at 30-50% if you can wrangle a set of % Add. Xp gear. The
+        maximum possible % Add. Xp stat is pushing 80-90% with all the OSBs and
+        XP pistols.
       </p>
+      <h1>Perk line: Training Seminar</h1>
+      <h1>
+        <a
+          href="https://auno.org/ao/db.php?id=212190&patch=15000100"
+          className="aboutLink"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Training Seminar +1-7%
+        </a>
+      </h1>
+      <p className="guideText">General experience boost perk line.</p>
       <h1>Nano line: Experience boost</h1>
       <h1>
         <a
@@ -22,32 +37,6 @@ function Exp() {
         </a>
       </h1>
       <p className="guideText">Bureaucrat experience boost nano line.</p>
-      <h1>
-        <a
-          href="https://auno.org/ao/db.php?id=262883&patch=16030000"
-          className="aboutLink"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Union Blessing +5%
-        </a>
-      </h1>
-      <p className="guideText">
-        Pulsing aura buff from Unionist Foreman at 4 Holes. Clan only.
-      </p>
-      <h1>
-        <a
-          href="https://auno.org/ao/db.php?id=262882&patch=16030000"
-          className="aboutLink"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Union Buster +10%
-        </a>
-      </h1>
-      <p className="guideText">
-        Buff for killing Unionist Agitator Scarpese at 4 Holes. Omni only.
-      </p>
       <h1>Nano line: 550</h1>
       <h1>
         <a
@@ -62,6 +51,20 @@ function Exp() {
       <p className="guideText">
         Bureaucrat experience boost perk line. It is an aura applied to
         teammates.
+      </p>
+      <h1>Nano line: Shadowlands Maps</h1>
+      <h1>
+        <a
+          href="https://prk.tinkeringidiot.com/items/227514"
+          className="aboutLink"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Knowledge of The End +5%
+        </a>
+      </h1>
+      <p className="guideText">
+        Adventurer experience boost nano. This nano is rollable.
       </p>
       <h1>Weapons</h1>
       <h1>
@@ -79,7 +82,7 @@ function Exp() {
       </p>
       <h1>
         <a
-          href="https://auno.org/ao/db.php?id=124428"
+          href="https://prk.tinkeringidiot.com/items/500107"
           className="aboutLink"
           target="_blank"
           rel="noreferrer"
@@ -145,6 +148,120 @@ function Exp() {
         The lowest stage tinker pistol to buff % Add. Xp is not obtainable
         (Hollow Island) as of writing.
       </p>
+      <h1>Neck</h1>
+      <h1>
+        <a
+          href="https://auno.org/ao/db.php?id=262954&patch=18040500"
+          className="aboutLink"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Sided Token Boards 1-8%
+        </a>
+      </h1>
+      <p className="guideText">Obtained when signing a faction application.</p>
+      <h1>Finger</h1>
+      <h1>
+        <a
+          href="https://auno.org/ao/db.php?id=84145&patch=18040400"
+          className="aboutLink"
+          target="_blank"
+          rel="noreferrer"
+        >
+          IQ Ring 1-14%
+        </a>
+      </h1>
+      <p className="guideText">
+        Drops in mission chest loot, Cyborgs, and Drills.
+      </p>
+      <h1>
+        <a
+          href="https://auno.org/ao/db.php?id=226287&patch=15000100"
+          className="aboutLink"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Shadowlands Progression Rings (Sanguine Ring for the Artillery Unit)
+          1-5%
+        </a>
+      </h1>
+      <p className="guideText">
+        Given by Ergo to enter the Shadowlands zone. Right finger only.
+      </p>
+      <h1>Back</h1>
+      <h1>
+        <a
+          href="https://auno.org/ao/db.php?id=274702"
+          className="aboutLink"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Complete Prisoner Cloaks (Cloak of the Reanimated Gladiator (5/5)) 3%
+        </a>
+      </h1>
+      <p className="guideText">
+        Tradeskilled from Crypt of Home and Escaped Prisoner loot.
+      </p>
+      <h1>
+        <a
+          href="https://auno.org/ao/db.php?id=281468"
+          className="aboutLink"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Cultist Outfit 3%
+        </a>
+      </h1>
+      <p className="guideText">Dark Ruins solo reward.</p>
+      <h1>
+        <a
+          href="https://auno.org/ao/db.php?id=206136&patch=17070000"
+          className="aboutLink"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Charred Abaddon Chassis 2%
+        </a>
+      </h1>
+      <p className="guideText">
+        Inner Sanctum 2nd floor boss loot. Soldier only.
+      </p>
+      <h1>Implants</h1>
+      <p className="guideText">
+        % Add. Xp clusters can be found in Jobe Market. The vendor is closeby
+        the portal from Jobe Platform. They require psychology to craft. Numbers
+        in parenthesis are refined implants (201+).
+      </p>
+      <h1>
+        <a
+          href="https://auno.org/ao/db.php?id=172215&ql=151"
+          className="aboutLink"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Ear Implant 5-7(8)%
+        </a>
+      </h1>
+      <h1>
+        <a
+          href="https://auno.org/ao/db.php?id=194431&ql=110"
+          className="aboutLink"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Feet Implant 3-4(5)%
+        </a>
+      </h1>
+      <h1>
+        <a
+          href="https://auno.org/ao/db.php?id=192796&ql=110"
+          className="aboutLink"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Leg Implant 2-3%
+        </a>
+      </h1>
     </Stage>
   );
 }
