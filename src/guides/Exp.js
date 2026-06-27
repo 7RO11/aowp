20b0b191bb03384841b3854a54bab8982bb97b6d
7R011 <7R011@protonmail.ch>
2026-06-27 16:28:47 -0400

    xp 2

--- src/guides/Exp.js
+++ src/guides/Exp.js
@@ -13,6 +13,19 @@ function Exp() {
         maximum possible % Add. Xp stat is pushing 80-90% with all the OSBs and
         XP pistols.
       </p>
+      <p className="guideText">
+        There is an{" "}
+        <a
+          href="https://www.ao-universe.com/guides/classic-ao/gameplay-guides-6/xp-boosters-4"
+          className="aboutLink"
+          target="_blank"
+          rel="noreferrer"
+        >
+          AO-Universe guide
+        </a>{" "}
+        for buffing % Add. Xp, but it is for the wrong patch version and doesn't
+        include PRK specific items.
+      </p>
       <h1>Perk line: Training Seminar</h1>
       <h1>
         <a
@@ -52,6 +65,20 @@ function Exp() {
         Bureaucrat experience boost perk line. It is an aura applied to
         teammates.
       </p>
+      <h1>Perk line: Commanding Presence</h1>
+      <h1>
+        <a
+          href="https://prk.tinkeringidiot.com/items/210929"
+          className="aboutLink"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Commanding Presence 10 +2%
+        </a>
+      </h1>
+      <p className="guideText">
+        Bureaucrat AAO/AAD aura that gains an xp buff at perk 10, or level 205.
+      </p>
       <h1>Nano line: Shadowlands Maps</h1>
       <h1>
         <a
@@ -67,6 +94,7 @@ function Exp() {
         Adventurer experience boost nano. This nano is rollable.
       </p>
       <h1>Weapons</h1>
+      <hr />
       <h1>
         <a
           href="https://auno.org/ao/db.php?id=124428"
@@ -144,11 +172,48 @@ function Exp() {
       <p className="guideText">
         Rollable pistol. Advy only. Can be dual wielded.
       </p>
+      <h1>
+        <a
+          href="https://auno.org/ao/db.php?id=165140"
+          className="aboutLink"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Nophex Plasma Destroyer II +2%
+        </a>
+      </h1>
+      <p className="guideText">Drops from Notum Soldier. Soldier only.</p>
+      <h1>
+        <a
+          href="https://auno.org/ao/db.php?id=165140"
+          className="aboutLink"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Reet-Tech Junior Urban Crossbow II +1-2%
+        </a>
+      </h1>
+      <p className="guideText">Rollable/common pool rifle-bow.</p>{" "}
+      <h1>
+        <a
+          href="https://auno.org/ao/db.php?id=274979"
+          className="aboutLink"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Reflex/Twitch Pistol +1-2%
+        </a>
+      </h1>
+      <p className="guideText">
+        Adventurer only pistol from Milky Way prisoners. Requires 2000/2100
+        pistol.
+      </p>
       <p className="guideText">
         The lowest stage tinker pistol to buff % Add. Xp is not obtainable
         (Hollow Island) as of writing.
       </p>
       <h1>Neck</h1>
+      <hr />
       <h1>
         <a
           href="https://auno.org/ao/db.php?id=262954&patch=18040500"
@@ -161,6 +226,7 @@ function Exp() {
       </h1>
       <p className="guideText">Obtained when signing a faction application.</p>
       <h1>Finger</h1>
+      <hr />
       <h1>
         <a
           href="https://auno.org/ao/db.php?id=84145&patch=18040400"
@@ -189,6 +255,7 @@ function Exp() {
         Given by Ergo to enter the next Shadowlands zone. Right finger only.
       </p>
       <h1>Back</h1>
+      <hr />
       <h1>
         <a
           href="https://auno.org/ao/db.php?id=274702"
@@ -215,6 +282,17 @@ function Exp() {
       <p className="guideText">Dark Ruins solo reward.</p>
       <h1>
         <a
+          href="https://auno.org/ao/db.php?id=160708&patch=14020100"
+          className="aboutLink"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Spirit Shroud of the Support Unit 1%
+        </a>
+      </h1>
+      <p className="guideText">Drops from Notum Scourges in Penumbra.</p>
+      <h1>
+        <a
           href="https://auno.org/ao/db.php?id=206136&patch=17070000"
           className="aboutLink"
           target="_blank"
@@ -226,7 +304,133 @@ function Exp() {
       <p className="guideText">
         Inner Sanctum 2nd floor boss loot. Soldier only.
       </p>
+      <h1>
+        <a
+          href="https://auno.org/ao/db.php?id=160708&patch=14020100"
+          className="aboutLink"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Standard Bureaucrat Suit 1-3%
+        </a>
+      </h1>
+      <p className="guideText">
+        Drops from dynas and chest loot. Bureaucrat only.
+      </p>
+      <h1>
+        <a
+          href="https://auno.org/ao/db.php?id=156538&patch=17060000"
+          className="aboutLink"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Universal Office Worker Suit 1%
+        </a>
+      </h1>
+      <p className="guideText">
+        Bureaucrat only suit from{" "}
+        <a
+          href="https://www.ao-universe.com/guides/classic-ao/tradeskill-guides-6/armor-5/bureaucrat-suit"
+          className="aboutLink"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Foremans.
+        </a>
+      </p>
+      <h1>Feet</h1>
+      <hr />
+      <h1>
+        <a
+          href="https://auno.org/ao/db.php?id=218947"
+          className="aboutLink"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Support Unit Crepuscule Boots 1%
+        </a>
+      </h1>
+      <p className="guideText">
+        Tradeskilled Shadowlands armor. Must be QL 151 or higher.
+      </p>
+      <h1>
+        <a
+          href="https://auno.org/ao/db.php?id=218947"
+          className="aboutLink"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Faithful/Chosen Bureaucrat Boots 1-3%
+        </a>
+      </h1>
+      <p className="guideText">Bureaucrat Tier 3 armor. Sided only.</p>
+      <h1>Shoulder</h1>
+      <hr />
+      <h1>
+        <a
+          href="https://auno.org/ao/db.php?id=269897"
+          className="aboutLink"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Shoulderpads of the Icy Wastes 1-2%
+        </a>
+      </h1>
+      <p className="guideText">
+        Unique Alappaa boss loot. Can be upgraded to equip 1 on each shoulder.
+      </p>
+      <h1>Chest</h1>
+      <hr />
+      <h1>
+        <a
+          href="https://auno.org/ao/db.php?id=246314&patch=17060000"
+          className="aboutLink"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Predator Armor Vest 1%
+        </a>
+      </h1>
+      <p className="guideText">Drops from predator dynas in Elysium.</p>
+      <h1>
+        <a
+          href="https://auno.org/ao/db.php?id=245510&patch=15030000"
+          className="aboutLink"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Omni Special Issue Grey Jacket 1%
+        </a>
+      </h1>
+      <p className="guideText">Omni startup quest reward.</p>
+      <h1>Arms</h1>
+      <hr />
+      <h1>
+        <a
+          href="https://auno.org/ao/db.php?id=165361&patch=16000000"
+          className="aboutLink"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Unbecoming Sleeves of Ljotur 1%
+        </a>
+      </h1>
+      <p className="guideText">Drops from Ljotur world boss.</p>
+      <h1>Head</h1>
+      <hr />
+      <h1>
+        <a
+          href="https://auno.org/ao/db.php?id=246311&patch=15050000"
+          className="aboutLink"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Spirit Ring of Self-Education 1-3%
+        </a>
+      </h1>
+      <p className="guideText">Drops from Hiathlin dynas in Elysium.</p>
       <h1>Implants</h1>
+      <hr />
       <p className="guideText">
         % Add. Xp clusters can be found in Jobe Market. The vendor is closeby
         the portal from Jobe Platform. They require psychology to craft. Numbers
